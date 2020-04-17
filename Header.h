9d19647f987058b0b44e2babf2cebc065e236046
irinagrigorjewa <irkagrigorjewa@gmail.com>
2020-04-17 23:46:16 +0300

    Lab1

---
+++ Header.h
@@ -0,0 +1,55 @@
+// Ћабораторна€ работа є1 по дисциплине ћ–«в»—
+// ќбъ€вление класса
+
+
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+class Algor {
+private:
+	struct container { bool arr[8]; }; //в классе есть вектор с контейнерами, контейнеры это 4-х разр€дное чмсло
+	vector<container> vect;
+
+	int firstN;
+	int secondN;
+	int Result;
+	bool firstNumber[4];
+	bool secondNumber[4];
+public:
+	void setVectValue();
+	void additional(bool[8], int);
+	void leftShift(bool[8]);
+	void showVector(int);
+	void push(bool[8]);
+	void showInitialCondition();
+	void showFirst();
+	void showSecond();
+	void showTable(int);
+	void setResult();
+	void showResult(int);
+	Algor() {
+		for (int i = 0; i < 4; i++) {
+			firstNumber[i] = 0;
+			secondNumber[i] = 0;
+		}
+		firstN = 0;
+		secondN = 0;
+		Result = 0;
+	};
+	Algor(int firstNum, int secondNum) {
+		firstN = firstNum;
+		secondN = secondNum;
+		for (int i = 3; i >= 0; i--) {
+			firstNumber[i] = firstNum % 2;
+			firstNum /= 2;
+			secondNumber[i] = secondNum % 2;
+			secondNum /= 2;
+		}
+		setVectValue();
+		setResult();
+	};
+};
+
